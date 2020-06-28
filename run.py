e537b1f20ebf43ab3b1b0e73d46cc7ea8dcc1742
Noble Mungu <mungunoble@gmail.com>
2020-06-28 23:59:29 +0300

    add login for user to login the app

--- run.py
+++ run.py
@@ -43,8 +43,36 @@ def main():
     print('\n')
 
     while True:
-        print("Use these short codes:nw - create a new user, lg-login.")
-        break
+        print("Use these short codes navigate through the app:\n nw - create a new user \n lg-login  \n ex-exit .")
+
+        short_codes = input("Enter your choice").lower().strip()
+        if short_code == 'ex':
+            break
+        elif short_code == 'nw' :
+            print("New Account")
+            print("-"*50)
+            first_name = input("Enter your First name:").strip()
+            last_name = input("Enter your last name:").strip()
+            password = input("Enter your password:").strip()
+            con_password =input("Confirm password:").strip()
+
+
+            save_user(create_user(first_name,last_name,password))
+        elif password == con_password :
+            print("Password doesnt match")
+        elif short_code =='lg' :
+            print("-"*50)
+            print(' ')
+            print("To log in enter your user name and password")
+            user_name = input("Enter user name:").strip()
+            password = str(input("Enter password:"))
+            user_exist == verify_user(user_name,password)
+
+
+
+
+
+
 
 
 
